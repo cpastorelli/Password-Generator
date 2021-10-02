dec98886b6889d821a2aa1f20851b39f7f9761bd
Caroline Pastorelli <caroline.pastorelli@gmail.com>
2021-10-02 16:22:50 -0700

    Made Function to validate number length

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passLength;
-var addUppercase;
-var addNumeric;
+//var passLength;
+//var addUppercase;
+//var addNumeric;
 var addSpecial;
 
 
@@ -12,25 +12,36 @@ var numericElem ="0123456789";
 var specialElem = "!@#$%^&**()";
 
 //click button to make a password
-//Now I get prompts
+function beginCriteria() {
+
+  //Prompt asking for password length
+  var passLength = getPassLength();
+  console.log("Password length: " + passLength); //take me out in the end
+//I have a number variable here. So I need to make sure something happens where I can make an array?
+//That way I can add it all together in the end
+
+
+
+}
+
+
 //Prompt 1: Length (8-128 characters)
-//validate correct numbers and parameters
-passLength = getPassLength();
-
-
-
-
 function getPassLength() {
-  var passLength = document.getElementById("passLength").value;
-  if (isNaN(passLength) || passLength < 8 || passLength > 128)
-  {
-    text = "Input not valid";
-  } else {
+ 
+  //As long as the lengthSelect variable is:
+  // 1. a number 
+  //2. is at least 8 characters long 
+  //3. no more than 128 characters long
+  do{
 
-    text = "Alright! You entered the number NUMBERVALUE HERE. Next question..."
-  }
+    var lengthSelect = prompt("How long of a password do you need?");
+    
+    //validate correct numbers and parameters
+  } while (isNaN(lengthSelect) || lengthSelect < 8 || lengthSelect > 128);
+  
+  console.log(isNaN(lengthSelect)); //Take me out in the end
 
-  return passLength;
+  return lengthSelect;
 }
 
 
@@ -41,9 +52,8 @@ function getPassLength() {
 
 
 
-//Prompt 2: Uppercase
-//validate Yes no response
-//Prompt 3: Numeric
+
+
 //validate yes no respones
 //prompt 4: Special Characters
 //validate yes no response
