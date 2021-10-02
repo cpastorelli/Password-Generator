f8f6859661dd5dc2f366310e6c57c857fbf433e3
Caroline Pastorelli <caroline.pastorelli@gmail.com>
2021-10-02 13:56:36 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+//var passLength;
+var addUppercase;
+var addNumeric;
+var addSpecial;
+
+
+var upperElem = "ABCDEFGHIJKLMNOPQURSTVWXYZ";
+var lowerElem = "abcdefghijklmnopqrstuvwxyz";
+var numericElem ="0123456789";
+var specialElem = "!@#$%^&**()";
+
+//click button to make a password
+//Now I get prompts
+//Prompt 1: Length (8-128 characters)
+//validate correct numbers and parameters
+function generateAPassword(){
+  var passLength = getPassLength();
+
+}
+
+
+
+
+function getPassLength() {
+  var passLength = document.getElementById("passLength").value;
+  if (isNaN(passLength) || passLength < 8 || passLength > 128)
+  {
+    text = "Input not valid";
+  } else {
+
+    text = "Alright! Next question..."
+  }
+
+}
+
+
+
+
+
+
+
+
+
+//Prompt 2: Uppercase
+//validate Yes no response
+//Prompt 3: Numeric
+//validate yes no respones
+//prompt 4: Special Characters
+//validate yes no response
+//All Prompts answered- password is generated to match criteria
+//display the password written on page or as alert
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
