4bd568e97e5de493fd231bd9637c33775c902b66
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2021-11-07 19:08:15 -0800

    Changed the names of variables for easier readability

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,24 +1,27 @@
-// Assignment Code
-//create potential password values, separated by user requirements
-const lower_case = "abcdefghijklmnopqrstuvwxyz";
-const upper_case = lower_case.toUpperCase();
+// Variables
+const passwordBase = "abcdefghijklmnopqrstuvwxyz";
+const upper_case = passwordBase.toUpperCase();
 const num_required = "0123456789";
 const sym_required = "!@#$%^&*()";
-const prompt1 = "Will you need any uppercase letters?";
-const prompt2 = "Do you want numbers in your password? ";
-const prompt3 = "Do you want special characters in your password?";
+
+const question1 = "Will you need any uppercase letters?";
+const question2 = "Do you want numbers in your password? ";
+const question3 = "Do you want special characters in your password?";
 const promptEnd = "\n type yes or no";
+
+//onclick event to call start password generation. 
+document.getElementById("generate").onclick = beginCriteria;
 
 //click button to make password criteria prompts
 function beginCriteria() {
   //Prompt asking for password length
   let passLength = getPassLength();
   //Prompt asking if uppercase letters are needed
-  let addUpper = setPreferences(prompt1, promptEnd);
+  let addUpper = setPreferences(question1, promptEnd);
   //Prompt asking if numbers are needed
-  let addNum = setPreferences(prompt2, promptEnd);
+  let addNum = setPreferences(question2, promptEnd);
   //Prompt asking is special characters are needed
-  let addSym = setPreferences(prompt3, promptEnd);
+  let addSym = setPreferences(question3, promptEnd);
   //Turning the string into an array and randomizing characters
   let thePasswordString = createPasswordString(addUpper, addNum, addSym, passLength);
   //cutting the string down to the length required
@@ -32,68 +35,66 @@ function beginCriteria() {
 function getPassLength() {
   do{
 
-    var lengthSelect = prompt("How long of a password do you need?");
+    var lengthOfPassword = prompt("How long of a password do you need?");
     //validate correct numbers and parameters
-  } while (isNaN(lengthSelect) || lengthSelect < 8 || lengthSelect > 128);
+  } while (isNaN(lengthOfPassword) || lengthOfPassword < 8 || lengthOfPassword > 128);
   
   //turn string input into number
-  lengthSelect = Number(lengthSelect);
+  lengthOfPassword = Number(lengthOfPassword);
   // return number
-  return lengthSelect;
+  return lengthOfPassword;
 }
 
 //all prompts: only thing that changes is the prompt.....
-function setPreferences(phrase1, phrase2){
+function setPreferences(question, yesNo){
   do{
 
-    var addSymb = prompt(phrase1 + phrase2);
-    addSymb = addSymb.toLowerCase();
+    var addCriteria = prompt(question + yesNo);
+    addCriteria = addCriteria.toLowerCase();
     //validate yes no response
-  }while(addSymb !== "no"  && addSymb !== "yes");
+  }while(addCriteria !== "no"  && addCriteria !== "yes");
 
   //return yes no string value
-  return addSymb;
+  return addCriteria;
 }
 
 //random number generator
-function createPasswordString(upper, num, sym, pLength){
+function createPasswordString(addUppercase, addNumber, addSymbol, pLength){
 
   //Create basic password string using only lowercase characters
- let basicPass = lower_case;
+ let potentialPassword = passwordBase;
  
- //if user typed yes, concat uppercase characters to basicPass
- if (upper === "yes") basicPass += upper_case;
- //if user typed yes, concat number characters to basicPass
- if (num === "yes")  basicPass += num_required;
- //if user typed yes, concat symbol characters to basicPass
- if (sym === "yes")  basicPass += sym_required;
+ //if user typed yes, concat uppercase characters to potentialPassword
+ if (addUppercase === "yes") potentialPassword += upper_case;
+ //if user typed yes, concat number characters to potentialPassword
+ if (addNumber === "yes")  potentialPassword += num_required;
+ //if user typed yes, concat symbol characters to potentialPassword
+ if (addSymbol === "yes")  potentialPassword += sym_required;
 
   // check if the length required for the password is at least 128 characters long
   // if not, add potential password to itself until length max has been achieved
- while (basicPass.length < 128 ) {
-    basicPass += basicPass;
+ while (potentialPassword.length < 128 ) {
+    potentialPassword += potentialPassword;
   }
 
  //Turn password string into an array
- let ArrayPass = Array.from(basicPass);
- 
- for(let i =0; i < ArrayPass.length; i++) {
+ let ArrayOfPassword = Array.from(potentialPassword);
+
+ //randomize the array
+ for(let i =0; i < ArrayOfPassword.length; i++) {
    //create a random number via number generator
-   let rand = Math.floor(Math.random()* ArrayPass.length - 1);
+   let rand = Math.floor(Math.random()* ArrayOfPassword.length - 1);
    //create a temporary value placeholder
-   let temp = ArrayPass[i];
+   let temp = ArrayOfPassword[i];
 
    // save value from random number index in array, and save to index i of array
-   ArrayPass[i] = ArrayPass[rand];
+   ArrayOfPassword[i] = ArrayOfPassword[rand];
    //have value in random number index now be temporary value placeholder
-   ArrayPass[rand] = temp;
+   ArrayOfPassword[rand] = temp;
   }
 
  //Turn array back into a string using join
- basicPass = ArrayPass.join("");
+ potentialPassword = ArrayOfPassword.join("");
  //return string value
- return basicPass;
+ return potentialPassword;
 }
-
-//onclick event to call start password generation. 
-document.getElementById("generate").onclick = beginCriteria;
